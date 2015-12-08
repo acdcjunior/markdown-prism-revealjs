d21f892dcdc48bcddac30de9d99e79d3694c9eac
acdcjunior <acdcjunior@gmail.com>
2015-12-08 00:09:19 -0200

    Moved prism code + added prism-setup code

---
+++ customization/prismjs/prism-setup.js
@@ -0,0 +1,11 @@
+var codes = document.querySelectorAll('code');
+for (var i = codes.length - 1; i >= 0; i--) {
+    var code = codes[i];
+    // every code block will have line numbers
+    code.className += " line-numbers";
+    // if no lang is specified, java is presumed
+    if (code.className.indexOf("language") === -1) {
+        code.className += " language-java";
+    }
+    Prism.highlightElement(code);
+}
