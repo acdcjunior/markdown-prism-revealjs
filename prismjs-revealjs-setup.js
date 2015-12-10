c7bb1b57aede5bfc3ec5d014bf2a73f7b8190f63
acdcjunior <acdcjunior@gmail.com>
2015-12-10 16:24:20 -0200

    Modified prism setup so it can be run on demand elsewhere

--- prismjs-revealjs-setup.js
+++ prismjs-revealjs-setup.js
@@ -1,11 +1,17 @@
-var codes = document.querySelectorAll('code');
-for (var i = codes.length - 1; i >= 0; i--) {
-    var code = codes[i];
-    // every code block will have line numbers
-    code.className += " line-numbers";
-    // if no lang is specified, java is presumed
-    if (code.className.indexOf("language") === -1) {
-        code.className += " language-java";
+MarkdownPrismRevealJS = window.MarkdownPrismRevealJS || {};
+
+MarkdownPrismRevealJS.prismSetup = function () {
+    var codes = document.querySelectorAll('code');
+    for (var i = codes.length - 1; i >= 0; i--) {
+        var code = codes[i];
+        // every code block will have line numbers
+        code.className += " line-numbers";
+        // if no lang is specified, java is presumed
+        if (code.className.indexOf("language") === -1) {
+            code.className += " language-java";
+        }
+        Prism.highlightElement(code);
     }
-    Prism.highlightElement(code);
-}
+};
+
+MarkdownPrismRevealJS.prismSetup();
